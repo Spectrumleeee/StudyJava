499fd5be7a2752e45c899485d6160ed0c549cf46
lgpjxjjxmumail <spectrumleeee@gmail.com>
2014-08-23 12:58:20 +0800

    Revert "Revert "update [encrypt]""
    This reverts commit 2a0d5ce3e4611cd004ca4046245ac6beccdd043e.

--- 2、workplace/encrypt/src/main/java/com/tplink/encrypt/lgp/gui/Window.java
+++ 2、workplace/encrypt/src/main/java/com/tplink/encrypt/lgp/gui/Window.java
@@ -17,6 +17,8 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
+import javax.swing.UIManager;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -33,6 +35,15 @@ public class Window extends JFrame {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
+            	
+                try {
+                    JFrame.setDefaultLookAndFeelDecorated(true);
+                    UIManager
+                            .setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            	
                 try {
                     Window frame = new Window();
                     frame.setVisible(true);
